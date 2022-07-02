a48a9a2dc1d57568c2a01a01dfa941e0aa71c4ec
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-07-02 19:30:10 -0300

    Create libftprintf.h

---
+++ libftprintf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/02 15:43:40 by fpeixoto          #+#    #+#             */
+/*   Updated: 2022/07/02 15:43:43 by fpeixoto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+#define LIBFTPRINTF_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+
+int ft_printf(const char *s, ...);
+
+#endif
