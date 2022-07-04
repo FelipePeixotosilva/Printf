04f45ad49262ef8b11632ce2b9c8c3bf4d142bb7
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-07-04 19:24:50 -0300

    Update ft_endpointer.c

--- ft_endpointer.c
+++ ft_endpointer.c
@@ -6,11 +6,11 @@
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 20:58:14 by fpeixoto          #+#    #+#             */
-/*   Updated: 2022/07/03 21:10:59 by fpeixoto         ###   ########.fr       */
+/*   Updated: 2022/07/04 18:11:40 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libftprintf.h"
+#include "ft_printf.h"
 
 void ft_endpointer(unsigned long int nbr)
 {
@@ -37,5 +37,3 @@ void ft_endpointer(unsigned long int nbr)
     }
     
 }
-
-
