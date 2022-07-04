eb4be5c4e15d4a64da58979599669c0f659683e9
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-07-04 19:23:49 -0300

    Update ft_putstr.c

--- ft_putstr.c
+++ ft_putstr.c
@@ -1,27 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/03 18:34:53 by fpeixoto          #+#    #+#             */
-/*   Updated: 2022/07/03 18:35:40 by fpeixoto         ###   ########.fr       */
+/*   Created: 2022/07/02 15:43:40 by fpeixoto          #+#    #+#             */
+/*   Updated: 2022/07/04 19:06:41 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libftprintf.h"
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
 
-void ft_putstr(char *s)
-{
-    size_t i;
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
-    i = 0;
-    if(!s[i])
-        return ;
-    while (s[i])
-    {
-        write(1,&s[i],1);
-        i++;
-    }
-}
+
+int     ft_printf(const char *s, ...);
+void    ft_printnbr(int nbr);
+void    ft_printnbr_u(unsigned int nbru);
+size_t    ft_putstr(char *s);
+void    ft_printhexa(unsigned int nbr, char c);
+void    ft_endpointer(unsigned long int nbr);
+
+#endif
