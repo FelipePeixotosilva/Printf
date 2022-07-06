a5fb6f674c88f6c614d447723e1f8eeb2ab62765
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-07-05 21:49:23 -0300

    Update ft_printhexa.c

--- ft_printhexa.c
+++ ft_printhexa.c
@@ -6,20 +6,22 @@
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 19:08:47 by fpeixoto          #+#    #+#             */
-/*   Updated: 2022/07/04 18:25:26 by fpeixoto         ###   ########.fr       */
+/*   Updated: 2022/07/05 21:49:06 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void ft_printhexa(unsigned int nbr,char c)
+size_t ft_printhexa(unsigned int nbr,char c)
 {
     char cnbr;
+    static size_t i;
     
+    i = 0;
     if(nbr >= 0 && nbr <= 9)
     {
         cnbr = nbr + '0';
-        write(1,&cnbr,1);    
+        i = i + write(1,&cnbr,1);    
     }
     else if(nbr >= 10 && nbr <= 15)
     {
@@ -28,11 +30,12 @@ void ft_printhexa(unsigned int nbr,char c)
         else
             cnbr = nbr + 55;
             
-        write(1,&cnbr,1);        
+        i = i + write(1,&cnbr,1);        
     }
     else
     {
         ft_printhexa(nbr / 16,c);
         ft_printhexa(nbr % 16,c);
     }
+    return i;
 }
