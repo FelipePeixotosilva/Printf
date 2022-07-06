ed47a0285a7172b12a66d721ac8dcf88ead8c0f0
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-07-06 20:16:24 -0300

    Update ft_printnbr.c

--- ft_printnbr.c
+++ ft_printnbr.c
@@ -6,34 +6,47 @@
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 18:09:11 by fpeixoto          #+#    #+#             */
-/*   Updated: 2022/07/05 21:42:18 by fpeixoto         ###   ########.fr       */
+/*   Updated: 2022/07/06 20:06:35 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static size_t cchar(int n)
+{
+    size_t i;
+
+    i = 0;
+    if(n < 0)
+    {
+        i++;
+        n = n * -1;
+    }
+    while (n)
+    {   
+        i++;
+       n = n / 10; 
+    }
+    return i;
+}
 size_t ft_printnbr(int nbr)
 {
     char cnbr;
-    static size_t i;
-
-    i = 0;
+    
     if (nbr < 0)
     {
         write(1,"-",1);
         nbr = nbr * -1;
-        i++;
     }
     if(nbr < 10)
     {
         cnbr = nbr + '0';
         write(1,&cnbr,1);
-        i++;
     }
     else 
     {
         ft_printnbr(nbr/10);
         ft_printnbr(nbr%10);
     }
-    return i;
+    return cchar(nbr);
 }
