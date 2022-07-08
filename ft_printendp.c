c42fa900088ee00ebd7e197a6764e7e70d1fd347
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-07-08 17:35:16 -0300

    Update ft_printendp.c

--- ft_printendp.c
+++ ft_printendp.c
@@ -1,69 +1,67 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_endpointer.c                                    :+:      :+:    :+:   */
+/*   ft_printendp.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 20:58:14 by fpeixoto          #+#    #+#             */
-/*   Updated: 2022/07/08 12:50:55 by fpeixoto         ###   ########.fr       */
+/*   Updated: 2022/07/08 12:56:12 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static size_t ft_countc(unsigned long int  n)
+static size_t	ft_countc(unsigned long int n)
 {
-    size_t i;
+	size_t	i;
 
-    i = 0;
-    if(n == 0)
-    {
-        i++;
-    }
-    else
-    {
-        while (n)
-        {   
-        i++;
-        n = n / 16;
-        }
-    }
-    return i;
+	i = 0;
+	if (n == 0)
+	{
+		i++;
+	}
+	else
+	{
+		while (n)
+		{
+			i++;
+			n = n / 16;
+		}
+	}
+	return (i);
 }
 
-static size_t ft_convp(unsigned long int nbr)
+static size_t	ft_convp(unsigned long int nbr)
 {
-    char cnbr;
-  
-    if(nbr == 'x')
-    {
-        write(1,"x",1);
-    }
-    else if(nbr < 10)
-    {
-        cnbr = nbr + '0';
-        write(1,&cnbr,1);    
-    }
-    else if(nbr >= 10 && nbr <= 15)
-    {
-        cnbr = nbr + 87;
-        write(1,&cnbr,1);        
-    }
-    else
-    {
-        ft_convp(nbr/16);
-        ft_convp(nbr%16);
-    }
-    return ft_countc(nbr);
+	char	cnbr;
+
+	if (nbr == 'x')
+	{
+		write(1, "x", 1);
+	}
+	else if (nbr < 10)
+	{
+		cnbr = nbr + '0';
+		write(1, &cnbr, 1);
+	}
+	else if (nbr >= 10 && nbr <= 15)
+	{
+		cnbr = nbr + 87;
+		write(1, &cnbr, 1);
+	}
+	else
+	{
+		ft_convp(nbr / 16);
+		ft_convp(nbr % 16);
+	}
+	return (ft_countc(nbr));
 }
 
-size_t ft_endp(unsigned long int  n)
+size_t	ft_printendp(unsigned long int n)
 {
-    
-    if(n == 0)
-		return write(1,"(nil)",5);
-    else
-        return write (1, "0x", 2) + ft_convp(n);
-
+	if (n == 0)
+		return (write(1, "(nil)", 5));
+	else
+		return (write (1, "0x", 2) + ft_convp(n));
 }
