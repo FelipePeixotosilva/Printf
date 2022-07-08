e25f8ca18adabe77db8f77450b24358df47a0243
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-07-08 16:42:52 -0300

    Update ft_printhexa.c

--- ft_printhexa.c
+++ ft_printhexa.c
@@ -11,42 +11,43 @@
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-static size_t ft_countc(unsigned int  n)
-{
-    size_t i;
 
-    i = 0;
-    if(n == 0)
-        i++;
-    while (n)
-    {
-        i++;
-        n = n / 16;
-    }
-    return i;
-}
-size_t ft_printhexa(unsigned int nbr,char c)
+static size_t	ft_countc(unsigned int n)
 {
-    char cnbr;
-    
-   if(nbr < 10)
-    {
-        cnbr = nbr + '0';
-        write(1,&cnbr,1);    
-    }
-    else if(nbr >= 10 && nbr <= 15)
-    {
-        if(c == 'x')
-            cnbr = nbr + 87;
-        else
-            cnbr = nbr + 55;
-            
-        write(1,&cnbr,1);
-    }
-    else
-    {
-        ft_printhexa(nbr / 16,c);
-        ft_printhexa(nbr % 16,c);
-    }
-    return ft_countc(nbr);
+	size_t	i;
+
+	i = 0;
+	if (n == 0)
+	i++;
+	while (n)
+	{
+		i++;
+		n = n / 16;
+	}
+	return (i);
+}
+
+size_t	ft_printhexa(unsigned int nbr, char c)
+{
+	char	cnbr;
+
+	if (nbr < 10)
+	{
+		cnbr = nbr + '0';
+		write(1, &cnbr, 1);
+	}
+	else if (nbr >= 10 && nbr <= 15)
+	{
+		if (c == 'x')
+			cnbr = nbr + 87;
+		else
+			cnbr = nbr + 55;
+		write(1, &cnbr, 1);
+	}
+	else
+	{
+		ft_printhexa (nbr / 16, c);
+		ft_printhexa (nbr % 16, c);
+	}
+	return (ft_countc(nbr));
 }
