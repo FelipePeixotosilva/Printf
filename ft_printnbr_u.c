b76f3fec20d5c766b84b39f839ee83dc4e3390dd
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-07-08 17:01:14 -0300

    Update ft_printnbr_u.c

--- ft_printnbr_u.c
+++ ft_printnbr_u.c
@@ -6,41 +6,42 @@
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 18:23:30 by fpeixoto          #+#    #+#             */
-/*   Updated: 2022/07/06 23:26:03 by fpeixoto         ###   ########.fr       */
+/*   Updated: 2022/07/08 16:53:21 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-size_t ft_countc(unsigned int n)
+static size_t	ft_countc(unsigned int n)
 {
-    size_t i;
-    i = 0;
+	size_t	i;
 
-    if(n == 0)
-    {
-      i++;
-    }
-    while(n)
-    {
-      i++;
-      n = n / 10;
-     }
-   return i;
+	i = 0;
+	if (n == 0)
+	{
+		i++;
+	}
+	while (n)
+	{
+		i++;
+		n = n / 10;
+	}
+	return (i);
 }
-size_t ft_printnbr_u(unsigned int nbru)
+
+size_t	ft_printnbr_u(unsigned int nbru)
 {
-    char cnbr;
-    
-    if(nbru < 10)
-    {
-        cnbr = nbru + '0';
-        write(1,&cnbr,1);
-    }
-    else
-    {
-        ft_printnbr_u(nbru / 10);
-        ft_printnbr_u(nbru % 10);
-    }
-  return ft_countc(nbru);      
+	char	cnbr;
+
+	if (nbru < 10)
+	{
+		cnbr = nbru + '0';
+		write(1, &cnbr, 1);
+	}
+	else
+	{
+		ft_printnbr_u(nbru / 10);
+		ft_printnbr_u(nbru % 10);
+	}
+	return (ft_countc(nbru));
 }
