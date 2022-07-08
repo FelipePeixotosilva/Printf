9fe36b87b323d8e5b55a7025063fd519f55d54fc
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-07-08 13:05:04 -0300

    Update ft_putstr.c

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,26 +6,26 @@
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 18:34:53 by fpeixoto          #+#    #+#             */
-/*   Updated: 2022/07/05 21:10:37 by fpeixoto         ###   ########.fr       */
+/*   Updated: 2022/07/08 13:00:18 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-size_t ft_putstr(char *s)
+size_t	ft_putstr(char *s)
 {
-    size_t i;
+	size_t	i;
 
-    i = 0;
-    if(s == NULL)
-    {
-        write(1,"(null)",6);
-        return 6;
-    }
-    while (s[i])
-    {
-        write(1,&s[i],1);
-        i++;
-    }
-    return i;
+	i = 0;
+	if (s == NULL)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+	while (s[i])
+	{
+		write(1, &s[i], 1);
+		i++;
+	}
+	return (i);
 }
