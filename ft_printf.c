c97405c9afe33087dd81413d53f6180fbdd73282
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-07-08 17:34:40 -0300

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -1,67 +1,72 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printendp.c                                     :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/03 20:58:14 by fpeixoto          #+#    #+#             */
-/*   Updated: 2022/07/08 12:56:12 by fpeixoto         ###   ########.fr       */
+/*   Created: 2022/07/02 15:43:30 by fpeixoto          #+#    #+#             */
+/*   Updated: 2022/07/08 12:56:36 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static size_t	ft_countc(unsigned long int n)
+static int	checkv(char c, va_list arg)
 {
-	size_t	i;
+	size_t	count;
+	size_t	v;
+
+	count = 0;
+	if (c == 'c')
+	{
+		v = va_arg (arg, int);
+		count += write (1, &v, 1);
+	}
+	else if (c == 'i' || c == 'd')
+		count += ft_printnbr (va_arg (arg, int));
+	else if (c == '%')
+		count += write (1, &c, 1);
+	else if (c == 'u')
+		count += ft_printnbr_u (va_arg (arg, unsigned int));
+	else if (c == 's')
+		count += ft_putstr (va_arg (arg, char *));
+	else if (c == 'p')
+		count += ft_printendp(va_arg (arg, unsigned long int));
+	else if (c == 'x' || c == 'X')
+		count += ft_printhexa (va_arg (arg, unsigned int), c);
+	return (count);
+}
+
+int	ft_printf(const char *str, ...)
+{
+	va_list	arg;
+	int		i;
+	int		count;
 
 	i = 0;
-	if (n == 0)
+	count = 0;
+	va_start (arg, str);
+	while (str[i])
 	{
-		i++;
-	}
-	else
-	{
-		while (n)
+		if (str[i] == '%')
 		{
 			i++;
-			n = n / 16;
+			count += checkv (str[i], arg);
 		}
+		else
+		{
+			write(1, &str[i], 1);
+			count++;
+		}
+	i++;
 	}
-	return (i);
+	va_end (arg);
+	return (count);
 }
-
-static size_t	ft_convp(unsigned long int nbr)
+/*#include <stdio.h>
+int main()
 {
-	char	cnbr;
-
-	if (nbr == 'x')
-	{
-		write(1, "x", 1);
-	}
-	else if (nbr < 10)
-	{
-		cnbr = nbr + '0';
-		write(1, &cnbr, 1);
-	}
-	else if (nbr >= 10 && nbr <= 15)
-	{
-		cnbr = nbr + 87;
-		write(1, &cnbr, 1);
-	}
-	else
-	{
-		ft_convp(nbr / 16);
-		ft_convp(nbr % 16);
-	}
-	return (ft_countc(nbr));
-}
-
-size_t	ft_printendp(unsigned long int n)
-{
-	if (n == 0)
-		return (write(1, "(nil)", 5));
-	else
-		return (write (1, "0x", 2) + ft_convp(n));
-}
+	ft_printf(" %d ", -42);
+	printf(" %d ", -42);
+}*/
