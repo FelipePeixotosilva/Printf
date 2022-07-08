8a0d3ca20e44b69f3c3470e692cb161365fd2933
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-07-08 15:09:51 -0300

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -6,23 +6,22 @@
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 15:43:40 by fpeixoto          #+#    #+#             */
-/*   Updated: 2022/07/07 18:53:42 by fpeixoto         ###   ########.fr       */
+/*   Updated: 2022/07/08 12:56:25 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
-#define FT_PRINTF_H
+# define FT_PRINTF_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
 
-
-int     ft_printf(const char *s, ...);
-size_t  ft_printnbr(int nbr);
-size_t    ft_printnbr_u(unsigned int nbru);
-size_t    ft_putstr(char *s);
-size_t    ft_printhexa(unsigned int nbr, char c);
-size_t    ft_endpointer(unsigned long int nbr);
+size_t	ft_putstr(char *s);
+size_t	ft_printendp(unsigned long int n);
+size_t	ft_printhexa(unsigned int nbr, char c);
+size_t	ft_printnbr(int nbr);
+size_t	ft_printnbr_u(unsigned int nbru);
+int		ft_printf(const char *str, ...);
 
 #endif
